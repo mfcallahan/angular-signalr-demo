4f61b9ce97366670b4d72d12eb60a96f8718259b
Matthew Callahan <matthew.callahan@outlook.com>
2020-11-03 13:56:30 -0700

    configure cors

--- SignalrDemo.Server/SignalrDemo.Server/SignalrDemo.Server/Startup.cs
+++ SignalrDemo.Server/SignalrDemo.Server/SignalrDemo.Server/Startup.cs
@@ -25,6 +25,15 @@ namespace SignalrDemo.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                .WithOrigins("http://localhost:4200")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            });
+
             services.AddControllers();
         }
 
@@ -39,6 +48,8 @@ namespace SignalrDemo.Server
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
