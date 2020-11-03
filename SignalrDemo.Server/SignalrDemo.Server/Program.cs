1d91b648b1a85a890c3ac81c907de117cb49908b
Matthew Callahan <matthew.callahan@outlook.com>
2020-11-03 16:03:41 -0700

    server configured

--- SignalrDemo.Server/SignalrDemo.Server/Program.cs
+++ SignalrDemo.Server/SignalrDemo.Server/Program.cs
@@ -1,26 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace SignalrDemo.Server
 {
-    public class Program
+    public static class Program
     {
+        // This method is the application entry point; Main() is called when the app is started.
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        // ASP.NET Core apps configure and launch a host which responsible for app startup and lifetime management.
+        // At a minimum, the host configures a server and a request processing pipeline. The host can also set up
+        // logging, dependency injection, and configuration. See this link for more information:
+        // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/web-host?view=aspnetcore-3.1
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
--- SignalrDemo.Server/SignalrDemo.Server/Startup.cs
+++ SignalrDemo.Server/SignalrDemo.Server/Startup.cs
@@ -1,20 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using SignalrDemo.Server.Hubs;
 
 namespace SignalrDemo.Server
 {
+    // ASP.NET Core apps use a Startup class, which is named Startup by convention. The Startup class includes
+    // a Configure() method to create the app's request processing pipeline.  It can also include an optional
+    // ConfigureServices() method to configure the app's services. A "service" is a reusable component that provides
+    // app functionality. Services are registered in ConfigureServices and consumed across the app via dependency
+    // injection (DI) or ApplicationServices. ConfigureServices() and Configure() are called by the ASP.NET Core
+    // runtime when the application starts. See this link for more information:
+    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/startup?view=aspnetcore-3.1
     public class Startup
     {
+        // The Startup class is created by calling the WebHostBuilderExtensions.UseStartup<TStartup> method on
+        // the host builder inside the CreateHostBuilder() in Program.cs.
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,22 +25,28 @@ namespace SignalrDemo.Server
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // ConfigureServices() is called by the host before the Configure() method and will configure the app's
+        // services. By convention, this where configuration options are set, and where services are added the container.
+        // This method is optional for the Startup class.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy", builder => builder
-                .WithOrigins("http://localhost:4200")
+                .WithOrigins("http://localhost:4200") // the Angular app url
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials());
             });
 
             services.AddControllers();
+            services.AddSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Configure() is used to specify how the app responds to HTTP requests. The request pipeline is configured
+        // by adding middleware components to an IApplicationBuilder instance. IApplicationBuilder is available to the
+        // Configure method(), but it isn't registered in the service container. Hosting creates an IApplicationBuilder
+        // and passes it directly to Configure().
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -46,16 +55,13 @@ namespace SignalrDemo.Server
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseCors("CorsPolicy");
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<SignalrDemoHub>("/signalrdemohub");  // the SignalrDemoHub url
             });
         }
     }
