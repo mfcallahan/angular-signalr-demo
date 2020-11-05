159979600531039b16b59e88a0fc213ed856aff1
Matthew Callahan <matthew.callahan@outlook.com>
2020-11-05 14:00:02 -0700

    move signalr hub url to env config

--- SignalrDemo.Web/src/app/signalr.service.ts
+++ SignalrDemo.Web/src/app/signalr.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import * as signalR from '@microsoft/signalr';
 
 @Injectable({
@@ -17,7 +18,7 @@ export class SignalrService {
   public initiateSignalrConnection(): Promise<any>{
     return new Promise((resolve, reject) => {
       this.connection = new signalR.HubConnectionBuilder()
-        .withUrl('https://localhost:44333/signalrdemohub') // the SignalR server url as set in the .NET Project properties and Startup class
+        .withUrl(environment.signalrHubUrl) // the SignalR server url as set in the .NET Project properties and Startup class
         .build();
 
       this.setSignalrClientMethods();
