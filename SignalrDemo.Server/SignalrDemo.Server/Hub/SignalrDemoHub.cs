b8d0ce47b1c286555152444de001e6cde05066c4
Matt Callahan <matthew.callahan@outlook.com>
2022-09-29 11:30:34 -0700

    configure signalr, cors

---
+++ SignalrDemo.Server/SignalrDemo.Server/Hub/SignalrDemoHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalrDemo.Server.Interfaces;
+
+namespace SignalrDemo.Server.Hubs
+{
+    public class SignalrDemoHub : Hub<ISignalrDemoHub>
+    {
+        public SignalrDemoHub()
+        {
+
+        }
+
+        public void Hello()
+        {
+            Clients.Caller.DisplayMessage("Hello from the SignalrDemoHub!");
+        }
+    }
+}
+
+
+
--- SignalrDemo.Server/SignalrDemo.Server/Program.cs
+++ SignalrDemo.Server/SignalrDemo.Server/Program.cs
@@ -1,6 +1,30 @@
+// This is the main enry point to the application, Startup.cs and Program.cs are now unified and
+// leverage top-level statements, eliinating the need to declare a namespace, class, and main method:
+// https://docs.microsoft.com/en-us/aspnet/core/release-notes/aspnetcore-6.0?view=aspnetcore-6.0
+
+using SignalrDemo.Server.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure CORS
+var policyName = "defaultCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(policyName, builder =>
+    {
+        builder.WithOrigins("http://localhost:4200") // the Angular app url
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowAnyOrigin();
+    });
+});
+
+// Configure SignalR
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors(policyName);
+app.MapHub<SignalrDemoHub>("/RoutingHub");
 
 app.Run();
