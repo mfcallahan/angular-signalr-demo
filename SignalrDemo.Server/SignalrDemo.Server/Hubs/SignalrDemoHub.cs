de44c0dbd970947c81af32b275e11c519bcad48a
Matt Callahan <matthew.callahan@outlook.com>
2023-03-17 10:00:48 -0700

    server step 1

---
+++ SignalrDemo.Server/SignalrDemo.Server/Hubs/SignalrDemoHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalrDemo.Server.Interfaces;
+
+namespace SignalrDemo.Server.Hubs
+{
+    public class SignalrDemoHub : Hub<ISignalrDemoHub>
+    {
+        public void Hello()
+        {
+            Clients.Caller.DisplayMessage("Hello from the SignalrDemoHub!");
+        }
+    }
+}
---
+++ SignalrDemo.Server/SignalrDemo.Server/Program.cs
@@ -0,0 +1,24 @@
+using SignalrDemo.Server.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var policyName = "defaultCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(policyName, builder =>
+    {
+        builder.WithOrigins("https://localhost:4200") // the Angular app url
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowAnyOrigin();
+    });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseCors(policyName);
+app.MapHub<SignalrDemoHub>("/signalrdemohub");
+
+app.Run();
